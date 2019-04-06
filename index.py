c5fb059bb8dd7e69232473ec2d6ba190dfe5090e
loveprakon <loveprakon@gmail.com>
2019-04-06 16:15:30 +0700

    555

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
+#-*- coding=utf-8 -*-
 from flask import Flask
-import time
-print("new program")
-
 app = Flask(__name__)
-@app.route('/')
-def index():
-	return'<h1>Deployed to Heroku</h1>'
+@app.route("/")
+def hello():  #def  เป็นคำสำคัญสำหรับการสร้างฟังก์ชัน
+   return '<h3>Hello py</h3>' #ให้แสดงข้อความว่า Hello py ออกทางหน้าฟอร์ม
+
+if __name__ == '__main__':
+   app.run()
